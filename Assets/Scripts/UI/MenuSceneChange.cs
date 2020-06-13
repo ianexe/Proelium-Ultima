d88986895a320fd44bf6e1d5b1cac975bebd09b0
Ian Trueba <itrueba0809@gmail.com>
2020-06-13 17:14:17 +0200

    Menu FX in progress

---
+++ Assets/Scripts/UI/MenuSceneChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuSceneChange : MonoBehaviour
+{
+    public GameObject to_load;
+    public Button to_select;
+
+    public void ChangeScene()
+    {
+        if (to_load != null)
+            to_load.SetActive(true);
+        if (to_select != null)
+            to_select.Select();
+        transform.parent.gameObject.SetActive(false);
+    }
+}
