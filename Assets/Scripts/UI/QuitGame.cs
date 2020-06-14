7e9b4f2c5279faecfc72444c8f6f26d0b73ccf1f
Ian <itrueba0809@gmail.com>
2020-06-14 22:52:36 +0200

    New Build Ready

--- Assets/Scripts/UI/QuitGame.cs
+++ Assets/Scripts/UI/QuitGame.cs
@@ -6,6 +6,12 @@ public class QuitGame : MonoBehaviour
 {
     public void ExitGame()
     {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
